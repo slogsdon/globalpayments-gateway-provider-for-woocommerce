d87be4f503b93dd8ad01649c7253b27138d0a5e3
Shane Logsdon <shane@shanelogsdon.com>
2019-12-16 11:52:04 -0500

    improve test setup process

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -16,10 +16,17 @@ $wp_tests_dir = getenv( 'WP_TESTS_DIR' )
   ? getenv( 'WP_TESTS_DIR' )
   : '/tmp/wordpress-tests-lib';
 
+if ( ! is_dir( $wp_tests_dir ) ) {
+  exit( sprintf(
+    'WP Tests Library not found! Be sure to run `%s`',
+    "WP_TESTS_DIR='${wp_tests_dir}' ./tests/bin/install-wp-tests.sh wordpress_test \$WP_DB_USERNAME \$WP_DB_PASSWORD"
+  ) );
+}
+
 // Load test functions
 include_once $wp_tests_dir . '/includes/functions.php';
 
-tests_add_filter( 'muplugins_loaded', function () use ($plugin_dir) {
+tests_add_filter( 'muplugins_loaded', function () use ( $plugin_dir ) {
   include_once $plugin_dir . '/wp-content/plugins/woocommerce/woocommerce.php';
 } );
 
