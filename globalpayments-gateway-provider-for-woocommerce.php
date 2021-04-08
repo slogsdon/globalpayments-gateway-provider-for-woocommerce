628ffd6bccbcabde4aa48e31d947774d9428f4ac
apetrovici <adina.petrovici@endava.com>
2021-04-08 12:49:11 +0300

    TR-92: Error: Your plugin and author URIs are the same

--- globalpayments-gateway-provider-for-woocommerce.php
+++ globalpayments-gateway-provider-for-woocommerce.php
@@ -5,9 +5,8 @@
  * Description: This extension allows WooCommerce to use the available Global Payments payment gateways. All card data is tokenized using the respective gateway's tokenization service.
  * Version: 1.0.0
  * Requires PHP: 5.5.9
- * WC tested up to: 3.8.0
+ * WC tested up to: 5.0.0
  * Author: Global Payments
- * Author URI: https://github.com/globalpayments/globalpayments-gateway-provider-for-woocommerce
 */
 
 defined( 'ABSPATH' ) || exit;
